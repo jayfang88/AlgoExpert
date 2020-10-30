a59b4e8a5d993b2ccac67330fc60e855fb087437
jayfang88 <jayfang88@gmail.com>
2020-10-30 15:08:51 -0700

    edit validateSubsequence

--- validateSubsequence.js
+++ validateSubsequence.js
@@ -1,18 +1,16 @@
 // Easy Q
 
-// Time: O(n)   => n being length of array
+// Time: O(n)   => n being length of main array
 // Space: O(1)
 function isValidSubsequence(array, sequence) {
-    let top = sequence[0];
+    let sIdx = 0;
 
     for (num of array) {
-        if (num === top) {
-            sequence.shift();
-            top = sequence[0];
-        }
+        if (sIdx === sequence.length) return true;
+        if (num === sequence[sIdx]) sIdx++;
     }
 
-    return (sequence.length === 0);
+    return sIdx === sequence.length;
 }
 
 console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]));
