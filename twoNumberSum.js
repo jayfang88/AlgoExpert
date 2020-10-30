d55536a2042fcdd02dc5e723cc76ee8bf7b2fa5a
jayfang88 <jayfang88@gmail.com>
2020-10-30 14:35:41 -0700

    solve twoNumberSum

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -2,5 +2,16 @@
 
 
 function twoNumberSum(arr, target) {
-    let 
+    let positions = {};
+    for (i in arr) {
+        let num = arr[i];
+        let diff = target - num;
+        if (positions[diff] && positions[diff] !== i) return [num, diff];
+
+        positions[num] = i;
+    }
+
+    return [];
 }
+
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));     // => [-1, 11]
