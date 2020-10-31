e00715c0a459cd00e0a2c087557fc487aa03679f
jayfang88 <jayfang88@gmail.com>
2020-10-30 18:04:09 -0700

    add nodeDepths

---
+++ nodeDepths.js
@@ -0,0 +1,19 @@
+// Easy Q
+
+// Time: 
+// Space:
+function nodeDepths(root) {
+    let sum = 0;
+    let stack = [[root, 0]];
+
+    while (stack.length) {
+        let temp = stack.pop();
+        let node = temp[0];
+        let depth = temp[1];
+        sum += depth;
+        if (node.left) stack.push([node.left, depth + 1]);
+        if (node.right) stack.push([node.right, depth + 1]);
+    }
+
+    return sum;
+}
