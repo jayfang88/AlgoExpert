da48f5a959a353a3c0b5f833792fef5f1c93244c
jayfang88 <jayfang88@gmail.com>
2020-10-30 17:26:45 -0700

    update findClosestValueinBST

--- findClosestValueinBST.js
+++ findClosestValueinBST.js
@@ -1,17 +1,14 @@
 // Easy Q
 
-// Time: 
-// Space: 
+// Time: O(Log(n))  => n being total number of nodes
+// WORST Time: O(n) => tree just has one huge branch, answer is at the bottom
+// Space: O(1)
 function findClosestValueInBst(tree, target) {
     let curr = tree;
-    let minDiff = Math.abs(curr.value - target);
-    let closest = curr.value;
+    let closest = curr;
 
     while (curr) {
-        if (Math.abs(curr.value - target) < minDiff) {
-            closest = curr.value;
-            minDiff = Math.abs(curr.value - target);
-        }
+        if (Math.abs(curr.value - target) < Math.abs(closest.value - target)) closest = curr;
 
         if (target === curr.value) {
             return target;
@@ -22,5 +19,6 @@ function findClosestValueInBst(tree, target) {
         }
     }
 
-    return closest;
+    return closest.value;
 }
+
