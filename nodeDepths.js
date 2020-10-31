1d3a3c49703d02b94b6678e683948eb4acf8982a
jayfang88 <jayfang88@gmail.com>
2020-10-30 18:18:52 -0700

    add recursive approach nodeDepths

--- nodeDepths.js
+++ nodeDepths.js
@@ -18,3 +18,9 @@ function nodeDepths(root) {
 
     return sum;
 }
+
+// Recursive Approach
+function nodeDepths(root, depth=0) {
+    if (!root) return 0;
+    return depth + nodeDepths(root.left, depth+1) + nodeDepths(root.right, depth+1);
+}
