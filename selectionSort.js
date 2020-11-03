7fd702c6bb5131173fceee477a4e57ec4cbd5db0
jayfang88 <jayfang88@gmail.com>
2020-11-03 09:45:18 -0800

    add selectionSort

---
+++ selectionSort.js
@@ -0,0 +1,10 @@
+// Easy Q
+
+// Time: 
+// Space: 
+function selectionSort(array) {
+    // Write your code here.
+}
+
+
+console.log(selectionSort([8, 5, 2, 9, 5, 6, 3]));      // => [2, 3, 5, 5, 6, 8, 9]
