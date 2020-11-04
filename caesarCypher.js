1300e9d7b28404ed9a3e15347c1fb5b99ca6d241
jayfang88 <jayfang88@gmail.com>
2020-11-04 14:05:50 -0800

    add caesarCypher

---
+++ caesarCypher.js
@@ -0,0 +1,19 @@
+// Easy Q
+
+// Time: O(n)
+// Space:  O(n)
+function caesarCipherEncryptor(string, key) {
+    const alpha = 'abcdefghijklmnopqrstuvwxyz';
+
+    let newStr = '';
+    for (let char of string) {
+        let oldIdx = alpha.indexOf(char);
+        let newIdx = (oldIdx+key) % 26;
+        newStr += alpha[newIdx];
+    }
+
+    return newStr;
+}
+
+
+console.log(caesarCipherEncryptor('xyz', 2));       // => 'zab'
