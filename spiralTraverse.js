30f04affcfe74d11efbfac36b0bf4fcbd03c0486
jayfang88 <jayfang88@gmail.com>
2020-11-11 15:27:32 -0800

    solve spiralTraverse

--- spiralTraverse.js
+++ spiralTraverse.js
@@ -3,9 +3,54 @@
 // Time: 
 // Space: 
 function spiralTraverse(array) {
-
+    let ans = [];
+    let top = 0;
+    let right = array[0].length;
+    let bottom = array.length;
+    let left = 0;
+    let direction = 'R';
+    let i = 0;
+    let j = 0;
+    
+    while (i >= top && i < bottom && j >= left && j < right) {
+        ans.push(array[i][j])
+        if (direction === 'R') {
+            if (j+1 === right) {
+                direction = 'D';
+                i++;
+                top++;
+            } else {
+                j++;
+            }
+        } else if (direction === 'D') {
+            if (i+1 === bottom) {
+                direction = 'L';
+                j--;
+                right--;
+            } else {
+                i++;
+            }
+        } else if (direction === 'L') {
+            if (j === left) {
+                direction = 'U';
+                i--;
+                bottom--;
+            } else {
+                j--;
+            }
+        } else if (direction === 'U') {
+            if (i === top) {
+                direction = 'R';
+                j++;
+                left++;
+            } else {
+                i--;
+            }
+        }
+    }
+    
+    return ans;
 }
-
 
 
 let arr1= [
@@ -17,3 +62,11 @@ let arr1= [
 
 console.log(spiralTraverse(arr1));
 // => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+let arr2 = [
+    [1, 2, 3],
+    [8, 9, 4],
+    [7, 6, 5]
+]
+console.log(spiralTraverse(arr2));
+// => [1, 2, 3, 4, 5, 6, 7, 8 9]
