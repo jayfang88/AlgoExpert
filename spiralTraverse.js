e95b0b77cab47ebdd461823369621eba1a8ec362
jayfang88 <jayfang88@gmail.com>
2020-11-11 15:10:11 -0800

    add spiralTraverse

---
+++ spiralTraverse.js
@@ -0,0 +1,19 @@
+// Medium Q
+
+// Time: 
+// Space: 
+function spiralTraverse(array) {
+
+}
+
+
+
+let arr1= [
+    [ 1,  2,  3, 4], 
+    [12, 13, 14, 5], 
+    [11, 16, 15, 6], 
+    [10,  9,  8, 7]
+];
+
+console.log(spiralTraverse(arr1));
+// => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
