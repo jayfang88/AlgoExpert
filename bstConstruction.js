f2bd0946f2f7b12026eca44210297e61c05c572d
jayfang88 <jayfang88@gmail.com>
2020-11-12 13:32:49 -0800

    update bstConstruction

--- bstConstruction.js
+++ bstConstruction.js
@@ -10,14 +10,28 @@ class BST {
     }
 
     insert(value) {
-        // Write your code here.
-        // Do not edit the return statement of this method.
-        let newNode = 
+        let newNode = new BST(value);
+
+        if (value < this.value) {
+            if (this.left) {
+                this.left.insert(value);
+            } else {
+                this.left = newNode;
+            }
+        } else {
+            if (this.right) {
+                this.right.insert(value);
+            } else {
+                this.right = newNode;
+            }
+        }
+
         return this;
     }
 
     contains(value) {
         let curr = this;
+        
         while (curr) {
             if (value === curr.value) {
                 return true;
