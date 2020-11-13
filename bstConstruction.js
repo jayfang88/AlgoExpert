8503f55eb82aba7c3e1ec387546376088f90b196
jayfang88 <jayfang88@gmail.com>
2020-11-12 16:02:08 -0800

    solve bstConstruction

--- bstConstruction.js
+++ bstConstruction.js
@@ -48,9 +48,45 @@ class BST {
         return false;
     }
 
-    remove(value) {
-        // Write your code here.
-        // Do not edit the return statement of this method.
+    remove(value, parentNode = null) {
+        let currNode = this;
+        while (currNode) {
+            if (value < currNode.value) {
+                parentNode = currNode;
+                currNode = currNode.left;
+            } else if (value > currNode.value) {
+                parentNode = currNode;
+                currNode = currNode.right;
+            } else {
+                if (currNode.left && currNode.right) {
+                    currNode.value = currNode.right.getMinValue();
+                    currNode.right.remove(currNode.value, currNode);
+                } else if (!parentNode) {
+                    if (currNode.left) {
+                        currNode.value = currNode.left.value;
+                        currNode.right = currNode.left.right;
+                        currNode.left = currNode.left.left;
+                    } else if (currNode.right) {
+                        currNode.value = currNode.right.value;
+                        currNode.left = currNode.right.left;
+                        currNode.right = currNode.right.right;
+                    }
+                } else if (parentNode.left === currNode) {
+                    parentNode.left = currNode.left ? currNode.left : currNode.right;
+                } else if (parentNode.right === currNode) {
+                    parentNode.right = currNode.left ? currNode.left : currNode.right;
+                }
+                break;
+            }
+        }
         return this;
+    }
+
+    getMinValue() {
+        let curr = this;
+        while (curr.left) {
+            curr = curr.left;
+        }
+        return curr.value;
     }
 }
